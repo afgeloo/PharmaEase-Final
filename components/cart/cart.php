8e4ff1beafefc243d0ff6d6cd27351bd8a3428f3
DsLaysonJr <121597181+DsLaysonJr@users.noreply.github.com>
2024-12-07 05:51:10 +0800

    created the cart and its functionality

--- components/cart/cart.php
+++ components/cart/cart.php
@@ -25,12 +25,12 @@ $result = $conn->query($sql);
 <html lang="en">
 <head>
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="sexualwellness.css?v=<?php echo time(); ?>">
+  <link rel="stylesheet" href="cart.css?v=<?php echo time(); ?>">
   <link rel="shortcut icon" type="image/png" href="/PharmaEase/PharmaEase-Final/assets/PharmaEaseLogo.png">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
   <link href="https://fonts.googleapis.com/css2?family=Varela+Round&display=swap" rel="stylesheet">
-  <title>PharmaEase - Sexual Wellness</title>
-  <script src="\PharmaEase\PharmaEase-Final\components\homepage\products.js"></script>
+  <title>PharmaEase - Shopping Cart</title>
+  <script src="\PharmaEase\PharmaEase-Final\components\cart\cart.js"></script>
 </head>
 <body>
 <div class="container">
@@ -68,8 +68,121 @@ $result = $conn->query($sql);
         </form>
       </div>
     </div>
+    <!-- Cart Segment -->
+    <div class="cart-container">
+      <h1>Shopping Cart</h1>
 
+      <div class="shopping-cart">
+
+        <div class="column-labels">
+          <label class="product-image">Image</label>
+          <label class="product-details">Product</label>
+          <label class="product-price">Price</label>
+          <label class="product-quantity">Quantity</label>
+          <label class="product-removal">Remove</label>
+          <label class="product-line-price">Total</label>
+        </div>
+
+        <div class="product">
+          <input type="checkbox" class="product-checkbox">
+          <div class="product-image">
+            <img src="https://s.cdpn.io/3/dingo-dog-bones.jpg">
+          </div>
+          <div class="product-details">
+            <div class="product-title">Dingo Dog Bones</div>
+            <p class="product-description">The best dog bones of all time. Holy crap. Your dog will be begging for these things! I got curious once and ate one myself. I'm a fan.</p>
+          </div>
+          <div class="product-price">12.99</div>
+          <div class="product-quantity">
+            <div class="quantity-control" data-quantity="">
+              <button class="quantity-btn" data-quantity-minus="">
+                <svg viewBox="0 0 409.6 409.6">
+                  <g>
+                    <g>
+                      <path d="M392.533,187.733H17.067C7.641,187.733,0,195.374,0,204.8s7.641,17.067,17.067,17.067h375.467 c9.426,0,17.067-7.641,17.067-17.067S401.959,187.733,392.533,187.733z" />
+                    </g>
+                  </g>
+                </svg>
+              </button>
+              <input type="number" class="quantity-input" data-quantity-target="" value="1" step="1" min="1">
+              <button class="quantity-btn" data-quantity-plus="">
+                <svg viewBox="0 0 426.66667 426.66667">
+                  <path d="m405.332031 192h-170.664062v-170.667969c0-11.773437-9.558594-21.332031-21.335938-21.332031-11.773437 0-21.332031 9.558594-21.332031 21.332031v170.667969h-170.667969c-11.773437 0-21.332031 9.558594-21.332031 21.332031 0 11.777344 9.558594 21.335938 21.332031 21.335938h170.667969v170.664062c0 11.777344 9.558594 21.335938 21.332031 21.335938 11.777344 0 21.335938-9.558594 21.335938-21.335938v-170.664062h170.664062c11.777344 0 21.335938-9.558594 21.335938-21.335938 0-11.773437-9.558594-21.332031-21.335938-21.332031zm0 0" />
+                </svg>
+              </button>
+            </div>
+          </div>
+          <div class="product-removal">
+            <button class="remove-product">
+              Remove
+            </button>
+          </div>
+          <div class="product-line-price">12.99</div>
+        </div>
+
+        <div class="product">
+          <input type="checkbox" class="product-checkbox">
+          <div class="product-image">
+            <img src="https://s.cdpn.io/3/large-NutroNaturalChoiceAdultLambMealandRiceDryDogFood.png">
+          </div>
+          <div class="product-details">
+            <div class="product-title">Nutro™ Adult Lamb and Rice Dog Food</div>
+            <p class="product-description">Who doesn't like lamb and rice? We've all hit the halal cart at 3am while quasi-blackout after a night of binge drinking in Manhattan. Now it's your dog's turn!</p>
+          </div>
+          <div class="product-price">45.99</div>
+          <div class="product-quantity">
+            <div class="quantity-control" data-quantity="">
+              <button class="quantity-btn" data-quantity-minus="">
+                <svg viewBox="0 0 409.6 409.6">
+                  <g>
+                    <g>
+                      <path d="M392.533,187.733H17.067C7.641,187.733,0,195.374,0,204.8s7.641,17.067,17.067,17.067h375.467 c9.426,0,17.067-7.641,17.067-17.067S401.959,187.733,392.533,187.733z" />
+                    </g>
+                  </g>
+                </svg>
+              </button>
+              <input type="number" class="quantity-input" data-quantity-target="" value="1" step="1" min="1">
+              <button class="quantity-btn" data-quantity-plus="">
+                <svg viewBox="0 0 426.66667 426.66667">
+                  <path d="m405.332031 192h-170.664062v-170.667969c0-11.773437-9.558594-21.332031-21.335938-21.332031-11.773437 0-21.332031 9.558594-21.332031 21.332031v170.667969h-170.667969c-11.773437 0-21.332031 9.558594-21.332031 21.332031 0 11.777344 9.558594 21.335938 21.332031 21.335938h170.667969v170.664062c0 11.777344 9.558594 21.335938 21.332031 21.335938 11.777344 0 21.335938-9.558594 21.335938-21.335938v-170.664062h170.664062c11.777344 0 21.335938-9.558594 21.335938-21.335938 0-11.773437-9.558594-21.332031-21.335938-21.332031zm0 0" />
+                </svg>
+              </button>
+            </div>
+          </div>
+          <div class="product-removal">
+            <button class="remove-product">
+              Remove
+            </button>
+          </div>
+          <div class="product-line-price">45.99</div>
+        </div>
+
+        <div class="totals">
+          <div class="totals-item">
+            <label>Subtotal</label>
+            <div class="totals-value" id="cart-subtotal">0.00</div>
+          </div>
+          <div class="totals-item">
+            <label>Tax (5%)</label>
+            <div class="totals-value" id="cart-tax">0.00</div>
+          </div>
+          <div class="totals-item">
+            <label>Shipping</label>
+            <div class="totals-value" id="cart-shipping">0.00</div>
+          </div>
+          <div class="totals-item totals-item-total">
+            <label>Grand Total</label>
+            <div class="totals-value" id="cart-total">0.00</div>
+          </div>
+          <button class="checkout" style="display: none;">Checkout</button>
+        </div>
+            
+        
+
+      </div>
+    </div>
      <!-- Footer  -->
-     <?php include "footer.php"; ?>
+     <?php include "../homepage/footer.php"; ?>
+</div>
 </body>
 </html>
