c0f81bd7058e640a1ab8a87acc846074fb7bbb1b
DsLaysonJr <121597181+DsLaysonJr@users.noreply.github.com>
2024-12-07 01:41:30 +0800

    setup phpmailer

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => '__root__',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
-        'reference' => '915c3d685f139cd260a07bf11e4bd8b3202c5ea5',
+        'reference' => '5a50e11054720eb81874ba1e5141d75dce2cc894',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -13,7 +13,7 @@
         '__root__' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => '915c3d685f139cd260a07bf11e4bd8b3202c5ea5',
+            'reference' => '5a50e11054720eb81874ba1e5141d75dce2cc894',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
